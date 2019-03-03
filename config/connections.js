bfa855669daada1cf019205052cf836bef7e8a19
heincm <chris.m.hein@gmail.com>
2019-03-02 23:55:27 -0500

    added logic for JAWSDB

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql');
-
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '5xfbHo4U',
-    database: 'burgers_db'
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '5xfbHo4U',
+        database: 'burgers_db'
+    });
+};
 
 module.exports = connection;
